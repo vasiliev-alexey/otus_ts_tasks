ab458b3a7608ef7cd7acee4ffe4f51632a225be7
vasiliev-alexey <alex_v81@mail.ru>
2021-04-28 18:31:29 +0300

    fix types operation

--- src/func/parallel.test.ts
+++ src/func/parallel.test.ts
@@ -30,7 +30,6 @@ describe('Parallel', () => {
 
     const result = await new Parallel(2).jobs(...jobs);
     expect(result).toEqual([1, 3, 2, 5, 4]);
-
   });
 
   [1, 2, 3, 4, 5].forEach((maxJobs) =>
